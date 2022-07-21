720e805f118bdf1aa7f2e27bac962178bbd11417
GeraldCode <82231832+geralD132001@users.noreply.github.com>
2022-07-21 18:21:19 -0500

    Update ProvinciaController.java

--- src/main/java/com/backend/springboot/app/ubigeo/controllers/ProvinciaController.java
+++ src/main/java/com/backend/springboot/app/ubigeo/controllers/ProvinciaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 
-@CrossOrigin(origins = { "https://ayllusinchi-app.azurewebsites.net" })
+@CrossOrigin(origins = { "https://ayllusinchis-app.azurewebsites.net" })
 @RestController
 public class ProvinciaController {
 
